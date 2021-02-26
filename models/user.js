0d3ae150332ea4a13a500db658d419a95f3c3f04
katiemackenbrock <katiemackenbrock@gmail.com>
2021-02-26 09:17:38 -0800

    wtf

--- models/user.js
+++ models/user.js
@@ -1,31 +1,20 @@
-const mongoose = require('mongoose');
-
-const options = {
-  timestamps: true,
-  id: false,
-  toJSON: {
-    virtuals: true,
-    transform: (_doc, userDocToReturn) => {
-      delete userDocToReturn.password;
-      return userDocToReturn;
-    }
-  }
-}
-
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, options);
-
-module.exports = mongoose.model('User', userSchema);
+const express = require('express');
+const db = require('../models');
+const router = express.Router();
+// POST /user/profile receive the name of a housewife and add it to the database
+router.post('/profile', (req, res) => {
+  db.user.findById(req.params.id, function (err, user) {
+    if (err) return res.status(500).json({ error: err.message })
+  })
+})
+// DELETE Favorited Housewife
+router.delete('/:id', (req, res) => {
+  console.log(req.params)
+  db.user.findByIdAndDelete(req.user.housewifeId)
+    .then(deleteInfo => {
+      res.status(204).json({ message: deleteInfo })
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message })
+    })
+})
